ccc61a32d8f937ae1377aadf2e9fcbc65c7b1a6c
BobTheStringBuilder <153329753+BobTheStringBuilder@users.noreply.github.com>
2024-03-03 15:02:22 -0500

    Update ShapeDodger.java

--- ShapeDodger/src/game/ShapeDodger.java
+++ ShapeDodger/src/game/ShapeDodger.java
@@ -87,6 +87,35 @@ class ShapeDodger extends Game {
         }
     }
 
+    // Inner class declaration
+    class Obstacle extends Polygon {
+        private final boolean isBig;
+
+        public Obstacle(Point[] inShape, Point inPosition, double inRotation, boolean isBig) {
+            super(inShape, inPosition, inRotation);
+            this.isBig = isBig;
+        }
+
+        public boolean isBig() {
+            return isBig;
+        }
+
+        public void paint(Graphics brush) {
+            if (isBig)
+                brush.setColor(Color.red);
+            else
+                brush.setColor(Color.green);
+            Point[] points = getPoints();
+            int[] xPoints = new int[points.length];
+            int[] yPoints = new int[points.length];
+            for (int i = 0; i < points.length; i++) {
+                xPoints[i] = (int) points[i].x;
+                yPoints[i] = (int) points[i].y;
+            }
+            brush.fillPolygon(xPoints, yPoints, points.length);
+        }
+    }
+    
     public static void main(String[] args) {
         ShapeDodger game = new ShapeDodger();
         game.repaint();
