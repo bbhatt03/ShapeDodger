9b9d16784a6ab7e970a690d9b301c5e7f15b3eb0
BobTheStringBuilder <153329753+BobTheStringBuilder@users.noreply.github.com>
2024-03-03 17:14:55 -0500

    Update ScoreTracker.java

--- ShapeDodger/src/game/ScoreTracker.java
+++ ShapeDodger/src/game/ScoreTracker.java
@@ -1,16 +1,33 @@
 package game;
 
+/**
+ * The ScoreTracker class represents a tracker for the player's score in the game.
+ * It keeps track of the player's current score and provides methods to increase the score.
+ */
 public class ScoreTracker {
-	private int score;
+    private int score;
 
+    /**
+     * Constructs a new ScoreTracker object with an initial score of zero.
+     */
     public ScoreTracker() {
         this.score = 0;
     }
 
+    /**
+     * Increases the player's score by the specified number of points.
+     *
+     * @param points the number of points to increase the score by
+     */
     public void increaseScore(int points) {
         score += points;
     }
 
+    /**
+     * Retrieves the current score of the player.
+     *
+     * @return the current score of the player
+     */
     public int getScore() {
         return score;
     }
